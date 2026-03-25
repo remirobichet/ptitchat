6cb538b7891c00505c6e2b68e4d333093f698941
Rémi Robichet <rrobichet@infinitestudio.fr>
2026-03-25 15:56:16 +0100

    fix: redirect to admin home when user already connected hit login page

--- pages/admin/login.vue
+++ pages/admin/login.vue
@@ -1,3 +1,11 @@
+<script setup lang="ts">
+const auth = useAuth();
+
+if (auth.isAuthenticated.value) {
+  await navigateTo("/admin");
+}
+</script>
+
 <template>
   <div class="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
     <div class="w-full max-w-sm">
