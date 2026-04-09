91f21f6be25e71b3b363ac5b76303f8755635239
Rémi Robichet <rrobichet@infinitestudio.fr>
2026-04-09 16:35:04 +0200

    chore: add version #4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,8 @@ import { execSync } from "node:child_process";
 const resolveGitCommitHash = () => {
   const commitHash =
     process.env.VERCEL_GIT_COMMIT_SHA?.slice(0, 7) ||
-    process.env.COMMIT_SHA?.slice(0, 7);
+    process.env.COMMIT_SHA?.slice(0, 7) ||
+    process.env.SOURCE_COMMIT?.slice(0, 7);
 
   if (commitHash) {
     return commitHash;
